4296c17170428e9afad9b680336df5960012ae5f
LucasGoncalves24 <lucasgoncalvesslash@gmail.com>
2024-11-20 21:50:00 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
-
+/*
 wss.on("connection", (ws) => {
     ws.on("error", console.error)
 
@@ -14,3 +14,14 @@ wss.on("connection", (ws) => {
 
     console.log("client connected")
 })
+*/
+wss.on("connection", (socket) => {
+    socket.on("message", (data) => {
+        // Transmitir a mensagem para todos os clientes conectados
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+});
